baed52d1428d85f24874ab142076462bdbac867a
Chris Nanney <cnanney@gmail.com>
2023-02-17 00:48:28 -0600

    Create icon components

--- src/lib/components/CalcResult.svelte
+++ src/lib/components/CalcResult.svelte
@@ -3,6 +3,8 @@
     import { resizeTypeSelectorIfPresent } from '$lib/util'
     import { tick } from 'svelte'
     import type { Writable } from 'svelte/store'
+    import Clipboard from './icons/Clipboard.svelte'
+    import DoubleChevronUp from './icons/DoubleChevronUp.svelte'
 
     export let calc: CalcResult,
         inputValueStore: Writable<string>,
@@ -47,26 +49,22 @@
         </div>
         <div class="ml-auto flex text-gray-400 dark:text-gray-400">
             {#if calc.send ?? true}
-                <a href={null} on:click={sendToInput}
-                   class="cursor-pointer hover:text-gray-700 dark:hover:text-gray-300"
-                   title="Send to input">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none"
-                         viewBox="0 0 24 24"
-                         stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                              d="M5 11l7-7 7 7M5 19l7-7 7 7"/>
-                    </svg>
+                <a
+                    href={null}
+                    on:click={sendToInput}
+                    class="h-3 w-3 cursor-pointer hover:text-gray-700 dark:hover:text-gray-300"
+                    title="Send to input"
+                >
+                    <DoubleChevronUp />
                 </a>
             {/if}
-            <a href={null} on:click={copyToClipboard}
-               class="ml-2 cursor-pointer hover:text-gray-700 dark:hover:text-gray-300"
-               title="Copy to clipboard">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none"
-                     viewBox="0 0 24 24"
-                     stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                          d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"/>
-                </svg>
+            <a
+                href={null}
+                on:click={copyToClipboard}
+                class="h-3 w-3 ml-2 cursor-pointer hover:text-gray-700 dark:hover:text-gray-300"
+                title="Copy to clipboard"
+            >
+                <Clipboard />
             </a>
         </div>
     </div>
--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -3,6 +3,9 @@
     import { getSyncedStore } from '$lib/stores'
     import { createPopoutWindow } from '$lib/util'
     import { urlTo } from '$lib/util.js'
+    import External from './icons/External.svelte';
+    import Moon from './icons/Moon.svelte';
+    import Sun from './icons/Sun.svelte';
 
     const darkModeEnabled = getSyncedStore('darkModeEnabled', false)
 
@@ -38,29 +41,18 @@
                     </div>
                 </div>
                 <div class="flex">
-                    <div class="flex items-center">
+                    <div class="flex items-center text-gray-500 dark:text-gray-200">
                         <button on:click={toggleDarkMode} aria-label="Toggle Dark Mode" type="button" class="w-5 h-5"
                                 title="Toggle Dark Mode">
                             {#if !$darkModeEnabled}
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                                     class="text-gray-500 dark:text-gray-200">
-                                    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-                                </svg>
+                                <Moon/>
                             {:else}
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                                     class="text-gray-500 dark:text-gray-200">
-                                    <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                                          fill-rule="evenodd" clip-rule="evenodd"></path>
-                                </svg>
+                                <Sun/>
                             {/if}
                         </button>
                         <button on:click={createPopoutWindow} type="button" class="w-5 h-5 ml-2" aria-label="Pop Out"
                                 title="Pop Out">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                                 class="text-gray-500 dark:text-gray-200">
-                                <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"/>
-                                <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"/>
-                            </svg>
+                            <External/>
                         </button>
                     </div>
                 </div>
