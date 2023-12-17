815067b95fdb1711fb03e499c813e3be4743aa13
Chris Nanney <cnanney@gmail.com>
2023-12-16 18:24:49 -0600

    Add test for mergeOverlappingRanges

--- src/tests/util.test.ts
+++ src/tests/util.test.ts
@@ -1,64 +1,77 @@
+import { mergeOverlappingRanges } from '$lib/util'
 import { compileSearchTokens, findTokenMatches } from '$lib/util/emoji'
 import { describe, expect, it } from 'vitest'
 
 describe('emoji util test', () => {
   it('creates search tokens', () => {
-    expect(compileSearchTokens('joy   "cat " ^grin|at$')).toMatchObject([
+    expect(compileSearchTokens('joy   "cat " ^grin|at$')).toEqual([
       'joy',
       'cat ',
       '^grin',
       'at$',
     ])
-    expect(compileSearchTokens('!"! arrow"')).toMatchObject([])
-    expect(compileSearchTokens('"! arrow"')).toMatchObject(['! arrow'])
-    expect(compileSearchTokens('=the')).toMatchObject(['the'])
-    expect(compileSearchTokens('!^the')).toMatchObject([])
-    expect(compileSearchTokens('^"!the "')).toMatchObject(['^!the '])
-    expect(compileSearchTokens('\'"red hair"')).toMatchObject(['red hair'])
-    expect(compileSearchTokens('="red hair"')).toMatchObject(['red hair'])
-    expect(compileSearchTokens('!"red hair"')).toMatchObject([])
+    expect(compileSearchTokens('!"! arrow"')).toEqual([])
+    expect(compileSearchTokens('"! arrow"')).toEqual(['! arrow'])
+    expect(compileSearchTokens('=the')).toEqual(['the'])
+    expect(compileSearchTokens('!^the')).toEqual([])
+    expect(compileSearchTokens('^"!the "')).toEqual(['^!the '])
+    expect(compileSearchTokens('\'"red hair"')).toEqual(['red hair'])
+    expect(compileSearchTokens('="red hair"')).toEqual(['red hair'])
+    expect(compileSearchTokens('!"red hair"')).toEqual([])
   })
 
   it('finds search token matches', () => {
     const s = 'the cat'
-    expect(findTokenMatches(s, compileSearchTokens('^th'))).toMatchObject([
-      [0, 1],
-    ])
-    expect(findTokenMatches(s, compileSearchTokens('the'))).toMatchObject([
-      [0, 2],
-    ])
-    expect(findTokenMatches(s, compileSearchTokens('!the'))).toMatchObject([])
-    expect(findTokenMatches(s, compileSearchTokens('!^the'))).toMatchObject([])
-    expect(findTokenMatches(s, compileSearchTokens('=the'))).toMatchObject([
-      [0, 2],
-    ])
-    expect(findTokenMatches(s, compileSearchTokens("'the"))).toMatchObject([
-      [0, 2],
-    ])
-    expect(findTokenMatches(s, compileSearchTokens('at$'))).toMatchObject([
-      [5, 6],
-    ])
-    expect(findTokenMatches(s, compileSearchTokens('!at$'))).toMatchObject([])
-    expect(findTokenMatches(s, compileSearchTokens('cat'))).toMatchObject([
-      [4, 6],
-    ])
-    expect(findTokenMatches(s, compileSearchTokens('"e c"'))).toMatchObject([
-      [2, 4],
-    ])
-    expect(findTokenMatches(s, compileSearchTokens('e c'))).toMatchObject([
+    expect(findTokenMatches(s, compileSearchTokens('^th'))).toEqual([[0, 1]])
+    expect(findTokenMatches(s, compileSearchTokens('the'))).toEqual([[0, 2]])
+    expect(findTokenMatches(s, compileSearchTokens('!the'))).toEqual([])
+    expect(findTokenMatches(s, compileSearchTokens('!^the'))).toEqual([])
+    expect(findTokenMatches(s, compileSearchTokens('=the'))).toEqual([[0, 2]])
+    expect(findTokenMatches(s, compileSearchTokens("'the"))).toEqual([[0, 2]])
+    expect(findTokenMatches(s, compileSearchTokens('at$'))).toEqual([[5, 6]])
+    expect(findTokenMatches(s, compileSearchTokens('!at$'))).toEqual([])
+    expect(findTokenMatches(s, compileSearchTokens('cat'))).toEqual([[4, 6]])
+    expect(findTokenMatches(s, compileSearchTokens('"e c"'))).toEqual([[2, 4]])
+    expect(findTokenMatches(s, compileSearchTokens('e c'))).toEqual([
       [2, 2],
       [4, 4],
     ])
     // Dupes are expected here, filtered out elsewhere
-    expect(findTokenMatches(s, compileSearchTokens('^th th'))).toMatchObject([
+    expect(findTokenMatches(s, compileSearchTokens('^th th'))).toEqual([
       [0, 1],
       [0, 1],
     ])
+    expect(findTokenMatches('ON! arrow', compileSearchTokens('!'))).toEqual([
+      [2, 2],
+    ])
+    expect(findTokenMatches('ON! arrow', compileSearchTokens('"! a"'))).toEqual(
+      [[2, 4]]
+    )
+  })
+})
+
+describe('general util tests', () => {
+  it('tests mergeOverlappingRanges', () => {
     expect(
-      findTokenMatches('ON! arrow', compileSearchTokens('!'))
-    ).toMatchObject([[2, 2]])
+      mergeOverlappingRanges([
+        [1, 3],
+        [2, 5],
+        [7, 9],
+      ])
+    ).toEqual([
+      [1, 5],
+      [7, 9],
+    ])
     expect(
-      findTokenMatches('ON! arrow', compileSearchTokens('"! a"'))
-    ).toMatchObject([[2, 4]])
+      mergeOverlappingRanges([
+        [1, 3],
+        [7, 9],
+        [2, 5],
+        [3, 4],
+      ])
+    ).toEqual([
+      [1, 5],
+      [7, 9],
+    ])
   })
 })
